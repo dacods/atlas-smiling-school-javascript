e272229586c4dc4a02359c8881385c97c9452863
dacods <dacods.21@gmail.com>
2024-12-03 19:20:11 -0600

    Got tutorial section to look the same as example but still no arrows

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
- $(document).ready(function() {
-          
+$(document).ready(function () {
 
-          $('#quotes-loader').show()
 
-          $.ajax({
-            url: 'https://smileschool-api.hbtn.info/quotes',
-            method: 'GET',
-          }).done(function(data) {
-            $('.quotes-carousel').empty();
+    $('#quotes-loader').show()
 
-            data.forEach((quote, index) => {
-                let activeClass = index === 0 ? ' active ' : '';
+    $.ajax({
+        url: 'https://smileschool-api.hbtn.info/quotes',
+        method: 'GET'
+    }).done(function (data) {
+        $('.quotes-carousel').empty();
 
-                $('.quotes-carousel').append(
-                    `
+        data.forEach((quote, index) => {
+            let activeClass = index === 0 ? ' active ' : '';
+
+            $('.quotes-carousel').append(
+                `
                         <div class="carousel-item ${activeClass}">
                             <div class="row mx-auto align-items-center">
                                 <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
@@ -29,72 +29,73 @@
                             </div>
                         </div>
                     `
-                );
-            });
-          });
-          $('.quotes-carousel').slick({
-                infinite: true,
-                autoScroll: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow_white_left.png" alt="Previous" /></button>',
-                nextArrow: '<button type="button" class="slick-next"><img src="images/arrow_white_right.png" alt="Next" /></button>',
-                responsive: [{
-                  breakpoint: 768,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }]
-              });
-
-              $('.loader').hide();
-
-        $('#tut-loader').show();
-
-        $.ajax({
-            url: 'https://smileschool-api.hbtn.info/popular-tutorials',
-            method: 'GET',
-        }).done(function(data) {
-            $('tut-carousel').empty();
-
-            let $current;
-            data.forEach((video, index) => {
-                if (index % 4 === 0) {
-                    $current = $('<div>', { class: 'carousel-item' });
-                    if (index === 0) {
-                        $current.addClass('active');
-                    }
-                    $current.append('<div class="row no-gutters"></div>');
-                    $('.tut-carousel').append($current);
-            }
-                let $card = $(
-                    `
-                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center">
-                            <div class="card">
-                                <img src="${video.thumb_url}" class="card-img-top" alt="Thumbnail">
-                                <div class="card-img-overlay text-center">
-                                    <img src="images/play.png" alt="Play button" width="64px" class="align-self-center play-overlay">
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title font-weight-bold">${video.title}</h5>
-                                    <p class="card-text text-muted">${video[` subtitle `]}</p>
-                                    <div class="creator d-flex align-items-center">
-                                        <img src="${video.author_pic_url}" alt="Creator" class="rounded circle"
-                                        <h6 class="pl-3 m-0 main-color">${video.author}</h6>
-                                    </div>
-                                    <div class="info pt-3 d-flex justify-content-between">
-                                        <div class="rating"> 
-                                            ${Array.from({ length: 5}, (_, i) => i < video.star ? '<img src="images/star_on.png" alt="star on" width="15px">' : '<img src="images/star_off.png" alt="star off" width="15px"').join('')}
-                                            </div>
-                                            <span class="main-color">${video.duration}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    `
-                );
-                $current.find('row').append($card);
-            });
+            );
         });
- });
+    });
+    $('.quotes-carousel').slick({
+        infinite: true,
+        autoScroll: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow_white_left.png" alt="Previous" /></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow_white_right.png" alt="Next" /></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+
+    $('.loader').hide();
+
+
+    $('#tut-loader').show(); // Shows the loader
+
+    $.ajax({
+        url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+        method: 'GET'
+    }).done(function (data) {
+        // Clear existing carousel
+        $('.tut-carousel').empty();
+
+        let $current;
+        data.forEach((video, index) => {
+            if (index % 4 === 0) {
+                $current = $('<div>', { class: 'carousel-item' });
+                if (index === 0) {
+                    $current.addClass('active');
+                }
+                $current.append('<div class="row no-gutters"></div>');
+                $('.tut-carousel').append($current);
+            }
+
+            const $card = $(`
+                <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center">
+                    <div class="card">
+                        <img src="${video.thumb_url}" class="card-img-top" alt="Video thumbnail">
+                        <div class="card-img-overlay text-center">
+                            <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title font-weight-bold">${video.title}</h5>
+                            <p class="card-text text-muted">${video["sub-title"]}</p>
+                            <div class="creator d-flex align-items-center">
+                                <img src="${video.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle">
+                                <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                            </div>
+                            <div class="info pt-3 d-flex justify-content-between">
+                                <div class="rating">
+                                    ${Array.from({ length: 5 }, (_, i) => i < video.star ? '<img src="images/star_on.png" alt="star on" width="15px">' : '<img src="images/star_off.png" alt="star off" width="15px">').join('')}
+                                </div>
+                                <span class="main-color">${video.duration}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `);
+            $current.find('.row').append($card);
+        });
+    });
+});
